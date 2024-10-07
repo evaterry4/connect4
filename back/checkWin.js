34ffac480d6a4f683f088f67f5d9cff38c8e5661
evaterry4 <bzt4em@virginia.edu>
2024-10-06 21:04:10 -0400

    updates before rearranging to get rid of checkWin

--- back/checkWin.js
+++ back/checkWin.js
@@ -2,14 +2,12 @@ const http = require('http');
 const url = require('url');
 
 const hostname = "127.0.0.1";
-const port = 4003;
+const port = 5001;
 const server = http.createServer();
 
 server.on('request', ( request, response) => {
-    let q = url.parse(request.url, true);
-
     response.writeHead(200, { 'Content-type': 'application/JSON'});
-    // response.end(JSON.stringify( handleRequest(q)));
+    response.end(JSON.stringify( 1));
 })
 
 server.listen(port, hostname, () => console.log('server running on ' + port + ' at ' + hostname))
--- back/gameState.js
+++ back/gameState.js
@@ -15,31 +15,21 @@ server.on('request', ( request, response) => {
 server.listen(port, hostname, () => console.log('server running on ' + port + ' at ' + hostname))
 
 function handleRequest(q) {
-    console.log(q);
     if(q.pathname == "/start"){
-        //start a new game
         game = new Game();
-        // response.writeHead(200, { 'Content-type': 'application/JSON' });
-        // response.end(JSON.stringify(board));
     } 
     else if(q.pathname == "/dropChip"){
         let colNum = Number(q.query['col']);
-        // logging here outputs to terminal
-        game.dropChip(colNum);
-        // return board;
+        let curPlayerColor = q.query['selectedPlayer'];
+        if((curPlayerColor == "red" && game.curPlayer == 0) || (curPlayerColor == "yellow" && game.curPlayer == 1)){
+            game.dropChip(colNum);
+        }       
     }
     else if(q.pathname == "/getGameState"){
         return game;
     }
     
     return game;
-    // else if(q.pathname == '/dropChip') {
-    //     let rollValue = Number(q.query['col']);
-        
-    //     if(isNaN(rollValue)) return; // exit if this isn't a number
-    //     game.addRoll(rollValue);
-    // }
-    // return board;
 }
 
 
@@ -61,14 +51,6 @@ class Game {
             if (this.board[row][col] === '') {
                 this.board[row][col] = this.curPlayer;
                 this.curPlayer = this.switchPlayer(this.curPlayer);
-                // if (checkWin(row, col)) {
-                //     alert(`${currentPlayer.toUpperCase()} wins!`);
-                //     resetGame();
-                // }
-                // check win
-                // change player
-               // currentPlayer = currentPlayer === 'red' ? 'yellow' : 'red'; 
-                // Switch player
                 return this.board;
             }
         }
